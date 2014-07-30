b39542a80770a3150248dfe0f9745130503488be
Zhong <zchen87@gmail.com>
2014-07-30 14:57:10 -0700

    add machine_setup.sh

---
+++ machine_setup.sh
@@ -0,0 +1,6 @@
+# Add keys of nginx
+deb http://nginx.org/packages/ubuntu/ codename nginx
+deb-src http://nginx.org/packages/ubuntu/ codename nginx
+
+sudo apt-get update
+sudo apt-get install -y nginx
