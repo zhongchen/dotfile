2272d12751290cebf55690366e8362db8d462ff0
zhong <zhong@sumologic.com>
2015-10-29 10:15:59 -0700

    add mac os stuff

--- MacSetup.sh
+++ MacSetup.sh
@@ -1,6 +1,13 @@
 ### setup Mac environment
 brew install tmux
 brew install macvim
+brew install curl git git-flow wget zinc
+brew install caskroom/cask/brew-cask
+brew cask install iterm2
+brew tap caskroom/versions
+brew cask install java7
+brew cask install sublime-text
+brew cask install intellij-idea
 
 # use macvim 
 sudo ln -sf /usr/local/bin/mvim /usr/local/bin/vi
@@ -9,6 +16,6 @@ hash -r
 
 ln -sf ~/dotfile/vimrc ~/.vimrc
 ln -sf ~/dotfile/tmux.conf ~/.tmux.conf
-ln -sf ~/dotfile/gitconfig ~/.gitconfig
+#ln -sf ~/dotfile/gitconfig ~/.gitconfig
 ln -sf ~/dotfile/vim ~/.vim
 
