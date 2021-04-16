30d7bdc370fdb375782c80577a52d76b76933f88
zhong <zhongchen@google.com>
2021-04-16 13:07:36 -0700

    minor update

--- MacSetup.sh
+++ MacSetup.sh
@@ -18,32 +18,26 @@ done
 ln -sf ~/dotfile/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfile/zshrc ~/.zshrc
 ln -sf ~/dotfile/gitconfig ~/.gitconfig
+ln -sf ~/dotfile/vimrc ~/.vimrc
+ln -sf ~/dotfile/vim ~/.vim
 
 if ! [[ -f "/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]]; then
     brew install zsh-syntax-highlighting
 fi
 
+
+
+# https://github.com/agnoster/agnoster-zsh-theme/issues/123
+# install powerline fonts
+# https://github.com/powerline/fonts
+
 echo "Done"
-# brew install macvim
-# brew install curl git git-flow wget zinc
-# brew install caskroom/cask/brew-cask
-# brew cask install iterm2
-# brew tap caskroom/versions
-# brew cask install sublime-text
 
 
-# ln -sf ~/dotfile/vimrc ~/.vimrc
-# ln -sf ~/dotfile/vim ~/.vim
 
 # # fast grep
 # brew install the_silver_searcher
 # brew install ripgrep
 
 
-# brew install python3
-# pip3 install --upgrade pip setuptools
-
-
-# #ruby
-# gem install neovim
 
